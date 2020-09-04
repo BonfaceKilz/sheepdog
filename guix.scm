da2acfc3191d0000470541ddfb9c138a0431b507
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-09-05 02:38:47 +0300

    Add initial generated hall files

---
+++ guix.scm
@@ -0,0 +1,30 @@
+(use-modules
+  (guix packages)
+  ((guix licenses) #:prefix license:)
+  (guix download)
+  (guix build-system gnu)
+  (gnu packages)
+  (gnu packages autotools)
+  (gnu packages guile)
+  (gnu packages guile-xyz)
+  (gnu packages pkg-config)
+  (gnu packages texinfo))
+
+(package
+  (name "guile-sheepdog")
+  (version "0.1")
+  (source "./guile-sheepdog-0.1.tar.gz")
+  (build-system gnu-build-system)
+  (arguments `())
+  (native-inputs
+    `(("autoconf" ,autoconf)
+      ("automake" ,automake)
+      ("pkg-config" ,pkg-config)
+      ("texinfo" ,texinfo)))
+  (inputs `(("guile" ,guile-3.0)))
+  (propagated-inputs `(("guile-redis" ,guile-redis)))
+  (synopsis "")
+  (description "")
+  (home-page "")
+  (license license:gpl3+))
+
---
+++ hall.scm
@@ -0,0 +1,55 @@
+(hall-description
+  (name "sheepdog")
+  (prefix "guile")
+  (version "0.1")
+  (author "Bonface Munyoki")
+  (copyright (2020))
+  (synopsis "")
+  (description "")
+  (home-page "")
+  (license gpl3+)
+  (dependencies `())
+  (files (libraries
+           ((scheme-file "sheepdog")
+            (directory
+              "sheepdog"
+              ((directory
+                 "monitor"
+                 ((compiled-scheme-file "monitor-error")
+                  (scheme-file "monitor-error")))
+               (compiled-scheme-file "bark")
+               (scheme-file "bark")))))
+         (tests ((directory
+                   "tests"
+                   ((log-file "bark")
+                    (test-result-file "bark")
+                    (scheme-file "bark")))))
+         (programs ((directory "scripts" ())))
+         (documentation
+           ((org-file "README")
+            (symlink "README" "README.org")
+            (text-file "HACKING")
+            (text-file "COPYING")
+            (directory
+              "doc"
+              ((texi-file "sheepdog")
+               (text-file "stamp-vti")
+               (info-file "guile-sheepdog")
+               (text-file ".dirstamp")
+               (texi-file "version")))
+            (text-file "NEWS")
+            (text-file "AUTHORS")
+            (text-file "ChangeLog")))
+         (infrastructure
+           ((scheme-file "guix")
+            (scheme-file "hall")
+            (directory
+              "build-aux"
+              ((text-file "mdate-sh")
+               (text-file "install-sh")
+               (text-file "missing")
+               (scheme-file "test-driver")
+               (tex-file "texinfo")))
+            (autoconf-file "configure")
+            (automake-file "Makefile")
+            (in-file "pre-inst-env")))))
