7821b7b86faac8d215282367cf4881f9f4db73e6
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-09-11 01:19:57 +0300

    sheedog: bark: Add new command
    * sheepdog/bark.scm:
    (call-command-with-output-error-to-string): Add new command that
    returns both stderr and stdout from a subprocess. See
    <https://www.draketo.de/software/guile-capture-stdout-stderr.html
    >

--- sheepdog/bark.scm
+++ sheepdog/bark.scm
@@ -8,3 +8,16 @@
          (str (read-line port)))
     (close-pipe port)
     str))
+
+(define (call-command-with-output-error-to-string cmd)
+  (let* ((err-cons (pipe))
+         (port (with-error-to-port (cdr err-cons)
+                 (λ () (open-input-pipe cmd))))
+         (_ (setvbuf (car err-cons) 'block
+             (* 1024 1024 16)))
+         (result (read-delimited "" port)))
+    (close-port (cdr err-cons))
+    (values
+     result
+     (read-delimited "" (car err-cons)))))
+
