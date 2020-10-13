a6efc31c17a1247f82de2bce1b942c7c82bde982
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-10-14 00:18:20 +0300

    sheepdog: bark: Add pipe-pair function
    * sheepdog/bark.scm (pipe-pair): Add new function for executing
    commands using execlp.

--- sheepdog/bark.scm
+++ sheepdog/bark.scm
@@ -1,9 +1,46 @@
 (define-module (sheepdog bark)
   #:use-module (sheepdog error)
-  #:use-module ((ice-9 popen))
+  #:use-module (rnrs io ports)
+  #:use-module (rnrs bytevectors)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 receive)
   #:use-module (ice-9 rdelim)
   #:export (run-job))
 
+
+; See:
+; https://wiki.gnu.tools/git/gnu-tools-wiki/tree/code/modules/email.scm
+(define (pipe-pair cmd)
+  "Run COMMAND as a separate process and return four values values: its PID,
+an output port to write on COMMAND's standard input, an input port to
+read COMMAND's standard output, and an output/error port to read
+COMMAND's standard error."
+  (let ((input (pipe))
+        (output (pipe))
+        (output/error (pipe)))
+    (match (primitive-fork)
+      (0 ;; Inside child-process
+       (dynamic-wind
+         (const #t)
+         (λ ()
+           (close-port (cdr input))
+           (close-port (car output))
+           (close-port (car output/error))
+           (dup2 (fileno (car input)) 0)
+           (dup2 (fileno (cdr output)) 1)
+           (dup2 (fileno (cdr output/error)) 2)
+           (apply execlp (car cmd) cmd))
+         (λ ()
+           (primitive-_exit 127))))
+      (pid
+       (close-port (car input))
+       (close-port (cdr output))
+       (close-port (cdr output/error))
+       (values pid
+               (cdr input)
+               (car output)
+               (car output/error))))))
 
 (define (call-command-with-output-error-to-string cmd)
   (let* ((err-cons (pipe))
