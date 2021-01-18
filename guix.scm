97016931e15f3a03f92be94a7d7cab3e60d82aa4
BonfaceKilz <me@bonfacemunyoki.com>
2021-01-19 02:11:24 +0300

    guix.scm: Create custom guile-redis pkg that's at v2.0.0.
    guile-redis v2.0.0 has proper support for pubsub commands.

--- guix.scm
+++ guix.scm
@@ -2,6 +2,7 @@
   (guix packages)
   ((guix licenses) #:prefix license:)
   (guix download)
+  (guix git-download)
   (guix build-system gnu)
   (gnu packages)
   (gnu packages autotools)
@@ -9,6 +10,21 @@
   (gnu packages guile-xyz)
   (gnu packages pkg-config)
   (gnu packages texinfo))
+
+(define my-guile-redis
+  (package
+    (inherit guile-redis)
+    (name "my-guile-redis")
+    (version "2.0.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/aconchillo/guile-redis")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1zk2x37lw6ygf7rwy71svnsian8lj51axpxmm66ah7dazn69swlm"))))))
 
 (package
   (name "guile-sheepdog")
@@ -22,7 +38,7 @@
       ("pkg-config" ,pkg-config)
       ("texinfo" ,texinfo)))
   (inputs `(("guile" ,guile-3.0)))
-  (propagated-inputs `(("guile-redis" ,guile-redis)
+  (propagated-inputs `(("guile-redis" ,my-guile-redis)
                        ("guile-readline" ,guile-readline)
                        ("guile-colorized" ,guile-colorized)))
   (synopsis "")
